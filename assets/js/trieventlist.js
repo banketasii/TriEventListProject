76b81bf6daa04603d82a399e9d553cdefa6a644d
George Banketas <banketasii76@gmail.com>
2018-02-22 09:21:40 -0500

    Added functionality to where correct icon displays when event is added

--- assets/js/trieventlist.js
+++ assets/js/trieventlist.js
@@ -1,14 +1,33 @@
 /* Tri Event List Project JS */
 var ul = $("ul");
-var delBtns = $(".del-btn");
+var delBtns = $(".del-event-btn");
 var lis = $("li");
-var txtbx = $("#txtbx-event");
-var openLI = '<li><span class="del-btn"><i class="fas fa-trash-alt"></i></span> ';
-var closeLI = '</li>';
-var addBtn = $(".add-btn");
+var txtbx = $("#event-lstbox-txtbox");
+var openLI = '<li><span class="del-event-btn"><i class="fas fa-trash-alt"></i></span> ';
+var closeLIbeg = '<img class="event-icon" id="';
+var closeLIend = '"></li>';
+var addBtn = $(".add-event-btn");
+//Gets the iconName for the <li> tag
+function GetIconString(input) {
+    var iconName;
+    input = input.toLowerCase();
+    if (input.includes("swim")) {
+        iconName = "swimIcon";
+    }
+    else if (input.includes("run") || input.includes("jog")) {
+        iconName = "runIcon";
+    }
+    else if (input.includes("bike") || input.includes("cycle")) {
+        iconName = "bikeIcon";
+    }
+    else {
+        iconName = "finishIcon";
+    }
+    return iconName;
+}
 //Delete specific exercises when the <span> is clicked
 //Adds eventListener to current and future <span> tags
-ul.on("click", ".del-btn", function (event) {
+ul.on("click", ".del-event-btn", function (event) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
@@ -18,7 +37,8 @@ ul.on("click", ".del-btn", function (event) {
 txtbx.on("keypress", function (event) {
     if (event.which === 13) {
         var txtInput = $(this).val().toString();
-        ul.append(openLI + txtInput + closeLI);
+        var icon = GetIconString(txtInput);
+        ul.append(openLI + txtInput + closeLIbeg + icon + closeLIend);
         $(this).val("");
     }
 });
